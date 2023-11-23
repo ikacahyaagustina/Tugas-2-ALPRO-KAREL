619b4f028a94d83a21e5c31b4400a5239f10a943
ikacahyaagustina <149259465+ikacahyaagustina@users.noreply.github.com>
2023-11-23 23:32:29 +0700

    Create hurdle4.py

---
+++ hurdle4.py
@@ -0,0 +1,27 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def putar_balik():
+    turn_right()
+    move()
+    turn_right()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    putar_balik()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+
+
